8cad8bbf82d154eba804578c8ab89f7292c96bdc
Ro <you@example.com>
2020-02-13 20:59:16 -0800

    asd

--- single/scripts/pymovecardi3.py
+++ single/scripts/pymovecardi3.py
@@ -21,12 +21,13 @@ from threading import Lock
 #   Global Variables.  We use global variables so the callbacks can
 #   see the state and pass information to the main (timer) loop.
 #
-goalpos = [0.0, 0.0]		# Goal position
-                                                        
-t      = 0.0                  # Current time (sec)
-cmdpos = [0.0,0.0]            # Current cmd position (rad)
-cmdvel = [0.0,0.0]            # Current cmd velocity (rad
-cmdtor = [0.0,0.0]            # Current cmd torque (Nm)
+goalpos = [0.0, 0.0, 0.0]		    # Goal position
+zeropos = [-0.39, -0.37, 0.44]
+                                                
+t      = 0.0                        # Current time (sec)
+cmdpos = [0.0, 0.0, 0.0]            # Current cmd position (rad)
+cmdvel = [0.0, 0.0, 0.0]            # Current cmd velocity (rad
+cmdtor = [0.0, 0.0, 0.0]            # Current cmd torque (Nm)
 
 # We don't need a mutex here, as there is a single parameter (which
 # hence will always be self-consistent!)
@@ -42,12 +43,22 @@ cmdtor = [0.0,0.0]            # Current cmd torque (Nm)
 def goalCallback(msg):
     # Simply save the new goal position.
     global goalpos
-   
-    goalpos[0] = msg.x	
-    goalpos[1] = msg.y		
+    
+    x = msg.x
+    y = msg.y
+    z = msg.z
+
+    t1 = -math.atan(y/x)
+    
+
+    goalpos[0] = t1	+ zeropos[0]
+    goalpos[1] = zeropos[1]
+    
+    goalpos[2] = zeropos[2]	
     # Report.
-    rospy.loginfo("x Moving goal to %6.3frad" % goalpos[0])
-    rospy.loginfo("y moving to %6.3frad" % goalpos[1])
+    #rospy.loginfo("motor 0 (#5) Moving goal to %6.3frad" % goalpos[0])
+    #rospy.loginfo("motor 1 (#6) moving to %6.3frad" % goalpos[1])
+    #rospy.loginfo("motor 2 (#1) moving to %6.3frad" % goalpos[1])
 
 
 #
@@ -64,13 +75,16 @@ if __name__ == "__main__":
     rospy.sleep(0.4)
 
     command_msg = JointState()
+    command_msg.name.append('Photon/5')
     command_msg.name.append('Photon/6')
-    command_msg.name.append('Photon/5') 
     command_msg.name.append('Photon/1')
    
-    command_msg.position.append(0.0)
-    command_msg.position.append(0.0)
-    command_msg.position.append(0.0)
+    #command_msg.position.append(0.0)
+    #command_msg.position.append(0.0)
+    #command_msg.position.append(0.0)
+    command_msg.position.append(zeropos[0])
+    command_msg.position.append(zeropos[1])
+    command_msg.position.append(zeropos[2])
 
     command_msg.velocity.append(0.0)
     command_msg.velocity.append(0.0)
@@ -79,6 +93,8 @@ if __name__ == "__main__":
     command_msg.effort.append(0.0)
     command_msg.effort.append(0.0)
     command_msg.effort.append(0.0)
+
+  
 
     # Find the starting position and use as an offset for the sinusoid.
     # This will block, but that's appropriate as we don't want to start
@@ -88,10 +104,12 @@ if __name__ == "__main__":
 
     # Initialize the parameters and state variables.  Do this before
     # the subscriber is activated (as it may run anytime thereafter).
-    goalpos = [0.0, 0.0, 0.0]
-
+    #goalpos = [0.0, 0.0, 0.0]
+    goalpos = zeropos[:]
+    print(msg)
     t      = 0.0
-    cmdpos = [msg.position[0]-.39, msg.position[1]-.39, 0.0+.44]
+    cmdpos = [msg.position[0],msg.position[1],msg.position[2]]
+    #cmdpos = zeropos[:]
     cmdvel = [0.0, 0.0, 0.0]
     cmdtor = [0.0, 0.0, 0.0]
 
@@ -104,9 +122,8 @@ if __name__ == "__main__":
     servo = rospy.Rate(100)
     dt    = servo.sleep_dur.to_sec()
     rospy.loginfo("Running the servo loop with dt %f" % dt)
-    f = open('lol.txt', 'w')
-    #f.write('what\n')
-    # bag = rosbag.Bag('test.bag', 'w')
+    #f = open('lol.txt', 'w')
+
     starttime = rospy.Time.now()	
     while not rospy.is_shutdown():
 	msg = rospy.wait_for_message('/hebiros/robot/feedback/joint_state', JointState);
@@ -115,19 +132,17 @@ if __name__ == "__main__":
         # Current time (since start)
         servotime = rospy.Time.now()
         t = (servotime - starttime).to_sec()
-        #s = String()
-        #s.data = str(msg.position[0])
-        #bag.write("pos",s )
+        
         #f.write("{} {} {}\n".format(t, msg.position[0], msg.position[1]))
 
         # Adjust the commands, effectively filtering the goal position
         # into the command position.  Note we only use a single
         # parameter (goalpos) which we read just once, so there is no
         # chance of self-inconsistency.  I.e. we don't need to mutex!
-        TIMECONSTANT = 0.7		# Convergence time constant
+        TIMECONSTANT = 0.7	         	# Convergence time constant
         LAMBDA       = 1.0/TIMECONSTANT # Convergence rate
         MAXVELOCITY  = 2.0              # Velocity magnitude limit
-        for i in range(2):
+        for i in range(3):
     	    cmdacc = - 3.0 * LAMBDA * cmdvel[i] - LAMBDA*LAMBDA* (cmdpos[i] - goalpos[i])
     	    cmdvel[i] = cmdvel[i] + dt * cmdacc
     	    if   (cmdvel[i] >  MAXVELOCITY):
