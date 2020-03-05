24ac1d161c6655e716c7ed78ac6f525ae6bf3c8c
rohanmirch <rohanmirch9@gmail.com>
2020-03-04 23:52:21 -0800

    adding chess intelligence v1

--- single/scripts/pymapping.py
+++ single/scripts/pymapping.py
@@ -17,7 +17,7 @@ from std_msgs.msg    import Float64, String
 from opencv_apps.msg import FaceArrayStamped
 from single.msg import Num
 from threading import Lock
-
+import pickle
 import os
 
 M = None
@@ -51,6 +51,12 @@ def apply(uv):
 if __name__ == "__main__":
     #rospy.init_node('pymapping')
     calibrate()
+
+    #Dump M object 
+    pickle_out = open("mapping_arr.pickle","wb")
+    pickle.dump(M, pickle_out)
+    pickle_out.close()
+
     apply(np.array([1573., 894.]))
 
    
